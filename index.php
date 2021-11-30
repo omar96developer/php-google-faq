4a8faad150ecb25169b86d2a775e5c9599af9190
omar96developer <omakino@hotmail.it>
2021-11-30 14:43:12 +0100

    0.0

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php 
+
+$qa = [
+    'question_1' => 'answer_1', 
+    'question_2' => 'answer_2',
+    'question_3' => 'answer_3'
+]
+
+
+?>
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <main>
+    
+    </main>
+</body>
+</html>
